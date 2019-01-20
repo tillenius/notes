64bec49ff28828756797415f10dbf61165f6b706
Martin Tillenius <martin.tillenius@gmail.com>
2019-01-20 22:53:30 +0100

    Initial commit: PowerShell profile.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,23 @@
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PSReadlineKeyHandler -Key Ctrl+a -Function BeginningOfLine
+Set-PSReadlineKeyHandler -Key Ctrl+e -Function EndOfLine
+Set-PSReadlineKeyHandler -Key Shift+Ctrl+a -Function SelectBackwardsLine
+Set-PSReadlineKeyHandler -Key Shift+Ctrl+e -Function SelectLine
+Set-PSReadlineKeyHandler -Key Shift+Delete -Function Cut
+Set-PSReadlineKeyHandler -Key Ctrl+Insert -Function Copy
+
+# Set environment variables for Visual Studio Command Prompt
+pushd "$env:VS140COMNTOOLS\..\..\vc\bin\amd64"
+cmd /c "vcvars64.bat & set" |
+foreach {
+  if ($_ -match "=") {
+    $v = $_.split("=")
+    set-item -force -path "ENV:\$($v[0])" -value "$($v[1])"
+  }
+}
+popd
+
+function prompt {
+    "$($ExecutionContext.SessionState.Path.CurrentLocation)> "
+}
+
