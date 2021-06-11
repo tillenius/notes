4bfa63d78ae1b66cfc970378d9722dcd1ed5ab91
Martin Tillenius <martin.tillenius@machinegames.com>
2021-06-11 10:19:56 +0200

    Added dawn color scheme to sublime text 3.

--- setup-sublime.ps1
+++ setup-sublime.ps1
@@ -1,2 +1,3 @@
 cp "Default (Windows).sublime-keymap" "$Env:AppData\Sublime Text 3\packages\user\"
 cp "Preferences.sublime-settings" "$Env:AppData\Sublime Text 3\packages\user\"
+cp "Dawn.tmTheme" "$Env:AppData\Sublime Text 3\packages\user\"
